996c4b1ceb1ecd3b13ad5799221ddb7d7f1fc00e
kawaura-dai <71888912+kawaura-dai@users.noreply.github.com>
2020-12-06 04:38:47 +0900

    修正

--- app/javascript/packs/memo.js
+++ app/javascript/packs/memo.js
@@ -2,12 +2,12 @@ function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("form"));
-    const XHR =new XMLHttpRequest();
-    XHR.open("POST","/posts", true);
-    XHR.responseType ="json";
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (XHR.status != 200){
+      if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       }
@@ -16,14 +16,17 @@ function memo() {
       const formText = document.getElementById("content");
       const HTML = `
         <div class="post" data-id=${item.id}>
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
           <div class="post-content">
           ${item.content}
           </div>
         </div>`;
       list.insertAdjacentHTML("afterend", HTML);
-      formText.Value = "";
+      formText.value = "";
     };
     e.preventDefault();
   });
-}
-window.addEventListener("load", memo);
+ }
+ window.addEventListener("load", memo);
